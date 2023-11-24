c36d6889d8dc2986086ae7549c79a7aec90f8a61
jorgoose <contact.jorgensen@gmail.com>
2023-11-24 00:03:46 -0600

    Init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://bdn-ak-ssl.bloomberg.com/software/trv/sotr130_7_80.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'bloomberg-terminal*'
+  silentArgs     = "/s maindir=`"$env:SystemDrive\Program Files\bloomberg`""
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+
+# Replace 'C:' with the system drive
+$uninstallPath = Join-Path $env:SystemDrive "\ProgramData\{D82FB535-3AF9-4AEF-877A-A310FD18F090}\sotrt.exe"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  silentArgs     = "REMOVE=TRUE /s"
+  validExitCodes = @(0)
+  file           = $uninstallPath
+}
+
+Uninstall-ChocolateyPackage @packageArgs
